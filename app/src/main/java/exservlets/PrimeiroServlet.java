b74dbce8cb12e411b0b44432a1e5907806f4d9db
Igor Galvão <igorgalvao092@gmail.com>
2023-04-18 17:39:43 +0000

    new

--- app/src/main/java/exservlets/PrimeiroServlet.java
+++ app/src/main/java/exservlets/PrimeiroServlet.java
@@ -2,10 +2,12 @@ package exservlet;
 
 import java.io.IOException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
 public class PrimeiroServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
         throws ServletException, IOException {
